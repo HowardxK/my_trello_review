acf2cb2959eac48a131cf2239b38820c206be407
howard <howard67890@gmail.com>
2021-07-25 11:57:16 +0800

    Vue.js 改寫

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -14,3 +9,22 @@ ActiveStorage.start()
 
 import 'scripts'
 import 'styles'
+
+// vue.js
+
+import Vue from 'vue/dist/vue.esm'
+
+document.addEventListener("turbolinks:load", function(event) {
+  let el = document.querySelector('#board')
+  
+  if (el) {
+    new Vue({
+      el, // es6 el: el key 跟 value 一樣 可以只寫一個就好
+      data: {
+        lists: JSON.parse(el.dataset.lists)
+      }
+    })
+  } else {
+
+  }
+})
