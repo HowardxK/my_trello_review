0241365701f05e8e6a42e7446eb70c9a23cf7ebc
howard <howard67890@gmail.com>
2021-07-26 02:46:43 +0800

    新增卡片 part 1

--- app/frontend/components/list.vue
+++ app/frontend/components/list.vue
@@ -5,11 +5,16 @@
       <!-- list.name 的 list 來自 props: ["list"] -->
 
       <div class="deck">
-        <Card v-for="card in list.cards" :card="card" :key="card.id"></Card>
+        <Card v-for="card in cards" :card="card" :key="card.id"></Card>
         <!-- 為了避免重複加上 :key="card.id" -->
         <!-- :card="card" 的 card 是在 v-for 定義的 card  -->
         <!-- :card 指的是在 card.vue 的 props: ["card"] 會將 card 傳給 :card -->
         <!-- 而 v-for 的 list.cards 來自  application.html.erb 的 data-lists="<%= @lists.to_json(include: :cards) %>" -->
+
+        <div class="input-area">
+          <textarea class="content" v-model="content"></textarea>
+            <button class="button" @click="createCard">新增卡片</button>
+        </div>
       </div>
     </div>
 </template>
@@ -17,6 +22,7 @@
 
 // js
 <script>
+import Rails from '@rails/ujs'     // 引入 rails 內建的 ajax
 import Card from 'components/card' // 引入 'components/card' 取名叫做 Card
 
 export default {
@@ -24,6 +30,37 @@ export default {
   props: ["list"],     // 設定一個叫做 list 的 property 但需要傳資料進來
   components: {
     Card               // 註冊 元件 Card: Card 前面可以自己取 後面是 import 進來的 Card ， es6 可以只寫一半 
+  },
+  data: function() {
+    return {
+      content: '',
+      cards: this.list.cards
+    }
+  },
+  methods: {
+    createCard(event) {
+      event.preventDefault()
+     
+      // 打 api
+      let data = new FormData()
+      data.append("card[list_id", this.list.id)
+      data.append("card[name]", this.content)
+
+      Rails.ajax({
+        url: '/cards',
+        type: 'POST',
+        data,
+        dataType: 'json',
+        success: resp => {
+          console.log(resp)
+          this.cards.push(resp)
+          this.content = ""
+        },
+        error: err => {
+          console.log(err)
+        }
+      })
+    }
   }
 }
 </script>
@@ -40,6 +77,26 @@ export default {
 
   .deck {
     @apply .mt-2;
+  }
+
+  .input-area {
+    @apply .mt-2;
+
+    .content {
+      @apply .w-full .p-2 .rounded-sm;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
+    
+    .button {
+      @apply .px-3 .py-1 .font-semibold .text-sm .bg-blue-300 .rounded;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
   }
 }
 </style>
