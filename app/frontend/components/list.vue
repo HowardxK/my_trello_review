c86e20ec49f5335f11bfb417b91f2ea144cccfdd
howard <howard67890@gmail.com>
2021-07-25 12:25:35 +0800

    元件化 part 1 (List)

---
+++ app/frontend/components/list.vue
@@ -0,0 +1,27 @@
+// html
+<template>
+    <div class="list">
+      <h2 class="header">{{ list.name }}</h2>
+    </div>
+</template>
+
+
+// js
+<script>
+export default {
+  name: 'List',
+  props: ["list"]     // 設定一個叫做 list 的 property 但需要傳資料進來
+}
+</script>
+
+// style
+// lang 加入 scss 巢狀功能，scoped 代表 css 只在這個 component 範圍內，不會污染到其他 css
+<style lang="scss" scoped>    
+.list {
+  @apply .bg-gray-300 .mx-2 .w-64 .rounded;
+
+  .header {
+    @apply .px-3 .py-1 .font-bold;
+  }
+}
+</style>
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -13,6 +13,7 @@ import 'styles'
 // vue.js
 
 import Vue from 'vue/dist/vue.esm'
+import List from 'components/list' // 引入 'components/list' 取名 List(可任取名)
 
 document.addEventListener("turbolinks:load", function(event) {
   let el = document.querySelector('#board')
@@ -22,6 +23,9 @@ document.addEventListener("turbolinks:load", function(event) {
       el, // es6 el: el key 跟 value 一樣 可以只寫一個就好
       data: {
         lists: JSON.parse(el.dataset.lists)
+      },
+      components: {
+        List    // 註冊 元件 List: List es6 可只寫一個
       }
     })
   } else {
